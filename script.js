b8bd76265aa4e74f4ca31662db53222a5c66a38a
amr1n <52592047+amr1n@users.noreply.github.com>
2021-02-07 22:31:45 +0600

    added click event to the number buttons

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+let numbers = document.querySelectorAll('.num');
+let display = document.querySelector('.result_display');
+let operators = document.querySelectorAll('.operator');
+
+let display_value;
+
+
 function add(a, b) {
 	return a + b;
 }
@@ -14,12 +21,25 @@ function divide(a, b) {
 	if (b != 0) {
 		return a / b;
 	}
-}
-
+} 
 
 function operate(a, b, operator) {
 	return operator(a,b);
 }
+
+
+numbers.forEach(num => {
+	num.addEventListener('click', () => {
+		if (display.textContent == 0) {
+			display.textContent = " ";
+			display_value = display.textContent += num.textContent;
+		}else {
+			display_value = display.textContent += num.textContent;
+		}
+		display.textContent = display_value;	
+	});
+});
+
 
 
 
