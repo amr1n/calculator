9eb8eea56eec338dc17a863711178af16da167b7
amr1n <52592047+amr1n@users.noreply.github.com>
2021-02-09 20:44:49 +0600

    added the evaluate function and found a bug which is calculator cant calculate more than one digit numbers

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let numbers = document.querySelectorAll('.num'); 
 let operators = document.querySelectorAll('.operator');
 let display = document.querySelector('.result_display');
+let equation_display = document.querySelector('.equation_display');
 let all_clear = document.querySelector('.clear');
 let equals = document.querySelector('.equals');
 
+
 let display_value;
+let equation;
 let firstOperand;
 let lastOperand;
 let operator_func;
@@ -36,11 +39,25 @@ function operate(a, b, operator) {
 	return operator(a, b);
 }
 
-function clear() {
-	display_value = 0;
-	display.textContent = display_value;
+function clear() { 
+	display.textContent = 0;
+	equation_display.textContent = 0;
 }
 
+
+function evaluate() {
+	if (operator) {
+		lastOperand = display_value;
+		display.textContent = " ";
+	}
+	
+	let a = Number(firstOperand);
+	let b = Number(lastOperand);
+	result = operate(a, b, operator_func);
+
+	display.textContent = result;
+	return result;
+}
 
 
 numbers.forEach(num => {
@@ -51,17 +68,24 @@ numbers.forEach(num => {
 		}else {
 			display_value = display.textContent += num.textContent;
 		}
+
+		if (equation_display.textContent == 0) {
+			equation_display.textContent = " ";
+		    equation = equation_display.textContent += num.textContent;
+		}else {
+			equation = equation_display.textContent += num.textContent;
+		}
+
+		equation_display.textContent = equation;
 		display.textContent = display_value;
 	});
 });
 
-operators.forEach(operator => {
-	operator.addEventListener('click', () => {
+operators.forEach(operator_sign => {
+	operator_sign.addEventListener('click', () => {
 		let data;
-		if (operator.key === undefined) {
-			data = operator.id;
-		}else {
-			data = operator.id;
+		if (operator_sign.key === undefined) {
+			data = operator_sign.id;
 		}
 
 		switch(data) {
@@ -92,16 +116,20 @@ operators.forEach(operator => {
 
 
 		}
+
 		if (display_value != undefined) {
-			display.textContent = display_value += operator;
-		}else{
-			return;
+			firstOperand = display_value;
+			display.textContent = " ";
 		}
 		
+		if (equation != undefined) {
+			equation_display.textContent = equation += operator;
+		}
+
 	});
 });
 
 all_clear.addEventListener('click', clear);
 
-//equals.addEventListener('click', evaluate);
+equals.addEventListener('click', evaluate);
 
